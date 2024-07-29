70eaf13d0fdd4814107294e33cb347e416f295bf
Stanislav Zverev <stzverev.uy@gmail.com>
2024-07-29 06:33:47 -0300

    feat(cache): enhance IIN caching mechanism with Sinks
    - Add Sinks for improved caching of IIN information
    - Implement @PostConstruct init method to save IIN info to cache
    - Remove in-method caching logic in favor of Sinks-based approach

--- src/main/java/org/stzverev/cardcostapi/service/cardinfoprovider/cache/IINCacheProvider.java
+++ src/main/java/org/stzverev/cardcostapi/service/cardinfoprovider/cache/IINCacheProvider.java
@@ -1,5 +1,6 @@
 package org.stzverev.cardcostapi.service.cardinfoprovider.cache;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.stzverev.cardcostapi.domain.entity.IINCacheEntity;
@@ -8,7 +9,7 @@ import org.stzverev.cardcostapi.service.cardinfoprovider.IINExtractor;
 import org.stzverev.cardcostapi.service.cardinfoprovider.IINInfo;
 import org.stzverev.cardcostapi.service.cardinfoprovider.IINInfoProvider;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
+import reactor.core.publisher.Sinks;
 
 import java.time.Duration;
 import java.util.Date;
@@ -28,6 +29,22 @@ public class IINCacheProvider implements IINInfoProvider {
 
     private final Duration expirationDuration;
 
+    private final Sinks.Many<IINInfo> cachePublisher = Sinks.many().replay().all();
+
+    @PostConstruct
+    public void init() {
+        cachePublisher.asFlux()
+                .flatMap(iinInfo -> iinCacheRepository.save(IINCacheEntity.builder()
+                                .IIN(iinExtractor.getIin(iinInfo.iinInfo()))
+                                .issuingCountry(iinInfo.country())
+                                .expireAt(new Date(System.currentTimeMillis() + expirationDuration.toMillis()))
+                                .build()
+                        )
+                        .doOnNext(iinCacheEntity -> log.info("iin is saved to cache: {}", iinCacheEntity))
+                        .onErrorContinue((throwable, o) -> log.error("Error saving IINCacheEntity", throwable)))
+                .subscribe();
+    }
+
     /**
      * Retrieves card information based on the card number from cache.
      * If information is not found in cache, it will be fetched be delegate iinInfoProvider and
@@ -43,17 +60,6 @@ public class IINCacheProvider implements IINInfoProvider {
                 .doOnNext(iinCacheEntity -> log.info("iin is fetched from cache: {}", iinCacheEntity))
                 .map(iinInfoProvider -> new IINInfo(cardNumber, iinInfoProvider.getIssuingCountry()))
                 .switchIfEmpty(iinInfoProvider.getCardInfoByNumber(cardNumber))
-                .doOnNext(IINInfo ->
-                        iinCacheRepository.save(IINCacheEntity.builder()
-                                        .IIN(iin)
-                                        .issuingCountry(IINInfo.country())
-                                        .expireAt(new Date(System.currentTimeMillis() + expirationDuration.toMillis()))
-                                        .build()
-                                )
-                                .subscribeOn(Schedulers.boundedElastic())
-                                .doOnNext(iinCacheEntity -> log.info("iin is saved to cache: {}", iinCacheEntity))
-                                .onErrorContinue((throwable, o) -> log.error("Error saving IINCacheEntity", throwable))
-                                .subscribe()
-                );
+                .doOnNext(cachePublisher::tryEmitNext);
     }
 }
